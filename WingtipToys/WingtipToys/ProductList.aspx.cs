ca7119d87c944924d070be52d19f80666f6dc22d
jurin-n <jurin.nakano@gmail.com>
2020-01-14 21:25:40 +0900

    first commit.

---
+++ WingtipToys/WingtipToys/ProductList.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI;
+using WingtipToys.Models;
+using System.Web.ModelBinding;
+
+namespace WingtipToys
+{
+    public partial class ProductList : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public IQueryable<Product> GetProducts([QueryString("id")] int? categoryId)
+        {
+            var _db = new WingtipToys.Models.ProductContext();
+            IQueryable<Product> query = _db.Products;
+            if (categoryId.HasValue && categoryId > 0)
+            {
+                query = query.Where(p => p.CategoryID == categoryId);
+            }
+            return query;
+        }
+    }
+}
