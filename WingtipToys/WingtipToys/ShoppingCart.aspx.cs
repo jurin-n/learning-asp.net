83a9cd6885ae408063662d80d7aa2da42aaf3d05
jurin-n <jurin.nakano@gmail.com>
2020-01-15 20:13:08 +0900

    追加。

---
+++ WingtipToys/WingtipToys/ShoppingCart.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using WingtipToys.Logic;
+
+namespace WingtipToys
+{
+    public partial class ShoppingCart : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public List<Models.CartItem> GetShoppingCartItems()
+        {
+            ShoppingCartActions actions = new ShoppingCartActions();
+            return actions.GetCartItems();
+        }
+    }
+}
