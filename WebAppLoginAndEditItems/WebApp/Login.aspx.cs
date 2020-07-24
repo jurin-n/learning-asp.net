3b5be6a72d4c0d41ea274fafeadef728560ca3c8
jurin-n <jurin.nakano@gmail.com>
2020-07-24 18:42:31 +0900

    ユーザ認証機能追加。(パスワード暗号化はまだ対応できてない。)

--- WebAppLoginAndEditItems/WebApp/Login.aspx.cs
+++ WebAppLoginAndEditItems/WebApp/Login.aspx.cs
@@ -18,6 +18,31 @@ namespace WebApp
         {
             Console.WriteLine(UserId.Text);
             Console.WriteLine(Password.Text);
+
+            if (!IsValid)
+            {
+                //バリデーションエラーの場合、処理せず終了。
+                return;
+            }
+
+            var result = Authentication.IdAndPasswordSignIn(UserId.Text, Password.Text);
+            switch (result)
+            {
+                case 0:
+                    break;
+                case -1:
+                default:
+                    break;
+                    /*
+                    case SignInStatus.Success:
+                        break;
+                    case SignInStatus.Failure:
+                    default:
+                        FailureText.Text = "無効なログインです";
+                        ErrorMessage.Visible = true;
+                        break;
+                    */
+            }
         }
     }
 }
